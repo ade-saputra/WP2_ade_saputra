77b78c09881bc4f690a32af433d934e18401924b
ade-saputra <daesaputra681@gmail.com>
2023-04-06 11:28:09 +0700

    Tugas Pertemuan 3 dan 4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+ {
+   echo "<h1>Perkenalkan</h1>";
+   echo"Nama saya Ade Saputra
+   Saya tingga di daerah Depok
+   olahraga yang saya sukai adalah
+   Berkuda";
+ }
+}
